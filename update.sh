fbfe799c1edd845584fa11e5396edc8993fd2aee
timothyclemans <timothy.clemans@gmail.com>
2014-05-12 20:51:50 -0700

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,11 +1,11 @@
-kill -9 $(pidof manage)
-kill -9 $(pidof python)
+#kill -9 $(pidof manage)
+#kill -9 $(pidof python)
 cd
 rm -rf core
 git clone https://github.com/ryanjam4/core.git
 cd core
 python manage.py syncdb
 sudo netstat -plnt
-echo $$
-sh /home/tim/core/runserver.sh &
-kill -9 $$
+#echo $$
+#sh /home/tim/core/runserver.sh &
+#kill -9 $$
