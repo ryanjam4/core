af142a1a0e1600876dcd2a1fe46c96f3a5c6f23b
timothyclemans <timothy.clemans@gmail.com>
2014-05-12 20:34:58 -0700

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,8 +1,15 @@
+kill -9 $(pidof manage)
 kill -9 $(pidof python)
 cd
 rm -rf core
+kill -9 $(pidof manage)
+kill -9 $(pidof python)
 git clone https://github.com/ryanjam4/core.git
 cd core
 python manage.py syncdb
-sleep 20
+kill -9 $(pidof manage)
+kill -9 $(pidof python)
+python manage.py runserver 0.0.0.0:8000
+kill -9 $(pidof manage)
+kill -9 $(pidof python)
 python manage.py runserver 0.0.0.0:8000
