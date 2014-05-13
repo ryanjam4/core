fe6e10226d1f52d5da93effc21c29dc3aad12c10
timothyclemans <timothy.clemans@gmail.com>
2014-05-13 01:05:10 -0700

    Update keep_running.py

--- keep_running.py
+++ keep_running.py
@@ -4,4 +4,4 @@ while True:
     output = os.popen('cd; netstat -lnt | grep 8000').read()
     if output == '':
         time.sleep(20)
-        os.system('cd; cd /home/tim/core/; python /home/tim/core/manage.py runserver 0.0.0.0:8000')
+        os.system(' cd /home/tim/core/; python /home/tim/core/manage.py runserver 0.0.0.0:8000')
