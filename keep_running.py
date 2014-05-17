b4f19fbc55eb140b9080240c3ed1f520f2a80fc4
timothyclemans <timothy.clemans@gmail.com>
2014-05-16 19:45:20 -0700

    Update keep_running.py

--- keep_running.py
+++ keep_running.py
@@ -2,12 +2,12 @@ while True:
     import os
     import time
     print 'Netstat'
-    output = os.popen('cd /; netstat -lnt | grep 8000').read()
+    output = os.popen('cd / && netstat -lnt | grep 8000').read()
     if output == '':
-        print 'Sleeping 20 seconds'
-        time.sleep(20)
+        print 'Sleeping 5 seconds'
+        time.sleep(5)
 
-        os.system(' cd /home/tim/; cd /home/tim/core/; python /home/tim/core/manage.py runserver 0.0.0.0:8000')
+        os.system(' cd /home/tim/ && cd /home/tim/core/ && python /home/tim/core/manage.py runserver 0.0.0.0:8000')
     else:
         print 'Sleeping 1 second'
         time.sleep(1)
