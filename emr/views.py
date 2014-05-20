e11c5b0e4a1afdd8ed46bc3cf63642a8be201fa4
timothyclemans <timothy.clemans@gmail.com>
2014-05-20 10:06:58 -0400

    Update views.py

--- emr/views.py
+++ emr/views.py
@@ -99,8 +99,13 @@ def list_users(request):
 
 @login_required
 def view_patient(request, user_id):
+    role = UserProfile.objects.get(user=request.user).role
+    
     from pain.models import PainAvatar
     user = User.objects.get(id=user_id)
+    # allowed viewers are the patient, admin/physician, and other patients the patient has shared to
+    if (not (request.user == user) or (role in ['admin', 'physician']) or (user in list(set([i.patient for i in Sharing.objects.filter(other_patient=user)])))):
+        return HttpResponse("Not allowed")
     if (not is_patient(user)):
         return HttpResponse("Error: this user isn't a patient")
     context = {'patient': user, 'user_role': UserProfile.objects.get(user=request.user).role, 'patient_profile': UserProfile.objects.get(user=user), 'problems': Problem.objects.filter(patient=user)}
