7fde31600ddc4e9e4a852123ec48cb35c6396817
timothyclemans <timothy.clemans@gmail.com>
2014-05-22 23:31:56 -0400

    Update views.py

--- emr/views.py
+++ emr/views.py
@@ -18,6 +18,25 @@ class AccessLogMiddleware(object):
             access_log = AccessLog(user=request.user, summary=request.path)
             access_log.save()
 
+from django.http import *
+from django.shortcuts import render_to_response,redirect
+from django.template import RequestContext
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login, logout
+ 
+def login_user(request):
+    logout(request)
+    username = password = ''
+    if request.POST:
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            if user.is_active:
+                login(request, user)
+                return HttpResponseRedirect('/')
+    return render_to_response('login.html', context_instance=RequestContext(request))
+
 def update(request):
     os.system('sh /home/tim/core/update.sh &')
     html = """
